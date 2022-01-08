bb4bf1f0d60821174d1e5123736aad96cb1f41f3
Tyler Zhu <tyler.zhu@berkeley.edu>
2022-01-08 02:29:10 -0800

    rewrite bot with commands to make cleaner.

--- wordle_bot.py
+++ wordle_bot.py
@@ -1,7 +1,8 @@
-import discord
 import random
 from settings import MYTOKEN
 from utils import WORDLEBANK
+
+from discord.ext import commands
 
 WORDS = WORDLEBANK
 WORDS_SET = set(WORDS)
@@ -55,7 +56,7 @@ class WordleGame:
             return (-1, f"You lost! The word was {self.word}.")  # loss
         return (
             0,
-            f"You still have {self.turns} "
+            f"You still have {6-self.turns} "
             + ("turn" if self.turns == 5 else "turns")
             + " left!",
         )
@@ -65,83 +66,145 @@ class WordleGame:
         return self.word
 
     def getHistory(self):
+        if self.history == "":
+            return "No guesses."
         return self.history
 
     def getLetters(self):
         return self.letters
 
 
-class WordleClient(discord.Client):
+class MyContext(commands.Context):
+    async def checkGame(self, guild_id):
+        return guild_id in self.gamess
+
+
+class WordleBot(commands.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.games = dict()
 
+    # Override superclass Context class with my own
+    async def get_context(self, message, *, cls=MyContext):
+        return await super().get_context(message, cls=cls)
+
     async def on_ready(self):
         print(f"Logged in as {self.user.name}")
 
-    async def on_message(self, message):
-        # don't respond to self
-        if message.author == self.user:
-            return
-        guild = message.guild
-        channel = message.channel
-        # If a game currently exists in server
-        if guild.id not in self.games:
-            if message.content == "$start_standard":
-                await message.channel.send("Starting standard game of Wordle...")
-                word = random.choice(WORDS)
+    async def on_command_error(self, ctx, error):
+        await ctx.send(f"Error: {error}")
 
-                host = self.user
-                self.games[guild.id] = WordleGame(host, guild.name, channel.name, word)
-                await message.channel.send(
-                    "Send in a guess with $guess followed by your guess. You have 6 guesses."
-                )
-            elif message.content == "$start_custom":
-                # TODO: add DM behavior
-                await message.channel.send("Starting custom game of Wordle...")
-                await message.channel.send(
-                    "Choose a 5-letter word and DM the Bot with it."
-                )
-                host = message.author
+    ## Custom functions
+    async def checkGame(self, guild_id: int):
+        return guild_id in self.games
+
+    async def deleteGame(self, guild_id: int):
+        if await self.checkGame(guild_id):
+            del self.games[guild_id]
+            return 0
+        return 1
+
+    async def addGame(self, guild_id: int, game: WordleGame):
+        if await self.checkGame(guild_id):
+            return False
         else:
-            game = self.games[guild.id]
-            if message.content == "$review":
-                await message.channel.send("Your guesses so far are:")
-                await message.channel.send(game.getHistory())
-            elif message.content == "$letters":
-                await message.channel.send("Your available letters are:")
-                for k, v in game.getLetters().items():
-                    if k == "open":
-                        await message.channel.send(
-                            f":white_circle: Open letters: {' '.join(v)}"
-                        )
-                    else:
-                        await message.channel.send(
-                            f":green_circle: Found letters: {' '.join(v)}"
-                        )
-            elif message.content == "$restart":
-                await message.channel.send(f"Restarting... Word was {game.endGame()}")
-                del self.games[guild.id]
-            elif message.content.startswith("$guess"):
-                guess = message.content.split()[1].strip().upper()
-                print(f"Attempted guess in [{guild.name}] was {guess}")
-                if len(guess) != 5:
-                    await message.channel.send("Guess invalid, needs to be 5 letters.")
-                    return
-                if guess not in WORDS_SET:
-                    await message.channel.send(
-                        "Guess invalid, needs to be real 5 letter word."
-                    )
-                    return
-                await message.channel.send(f"Your guess was: {guess}")
-                guess_result, response = game.process_guess(guess)
-                await message.channel.send(game.getHistory())
-                await message.channel.send(response)
-                if guess_result == -1 or guess_result == 1:
-                    # Game over
-                    del self.games[guild.id]
-                return
+            self.games[guild_id] = game
+
+    async def getGame(self, guild_id: int):
+        if await self.checkGame(guild_id):
+            return self.games[guild_id]
+        else:
+            return None
 
 
-client = WordleClient()
-client.run(MYTOKEN)
+bot = WordleBot(command_prefix="$")
+
+
+# @bot.slash_command()
+@bot.command()
+async def start(ctx, game_type: str):
+    """Starts a new Wordle game (solo, collab, custom) if one hasn't begun already.
+    """
+    gid = ctx.guild.id
+
+    if await bot.checkGame(gid):
+        await ctx.send(
+            "Game already started. End the game first before starting a new one."
+        )
+        return
+
+    if game_type == "collab":
+        await ctx.send("Starting standard game of Wordle...")
+        word = random.choice(WORDS)
+
+        host = ctx.author
+        await bot.addGame(gid, WordleGame(host, ctx.guild.name, ctx.channel.name, word))
+        await ctx.send("Send in a guess. You have 6 guesses.")
+    elif game_type == "solo":
+        await ctx.send("Feature not yet supported. Try something else!")
+    elif game_type == "custom":
+        await ctx.send("Feature not yet supported. Try something else!")
+    else:
+        await ctx.send(
+            f"Invalid game type chosen. Choose either solo, collab, or custom."
+        )
+
+
+# @bot.slash_command()
+@bot.command()
+async def review(ctx):
+    """Review your previous guesses."""
+    await ctx.send("Your guesses so far are:")
+    game = await bot.getGame(ctx.guild.id)
+    await ctx.send(game.getHistory())
+
+
+# @bot.slash_command()
+@bot.command()
+async def letters(ctx):
+    """Get which letters are still possible."""
+    game = await bot.getGame(ctx.guild.id)
+    await ctx.send("Your available letters are:")
+    for k, v in game.getLetters().items():
+        if k == "open":
+            await ctx.send(f":white_circle: Open letters: {' '.join(v)}")
+        else:
+            await ctx.send(f":green_circle: Found letters: {' '.join(v)}")
+
+
+# @bot.slash_command()
+@bot.command()
+async def guess(ctx, guess: str):
+    """Make a guess in a wordle game."""
+    guess = guess.upper()
+    print(f"Attempted guess in [{ctx.guild.name}] was {guess}")
+    if len(guess) != 5:
+        await ctx.send("Guess invalid, needs to be 5 letters.")
+        return
+    if guess not in WORDS_SET:
+        await ctx.send("Guess invalid, needs to be real 5 letter word.")
+        return
+    await ctx.send(f"Your guess was: {guess}")
+    gid = ctx.guild.id
+    game = await bot.getGame(gid)
+    guess_result, response = game.process_guess(guess)
+    await ctx.send(game.getHistory())
+    await ctx.send(response)
+    if guess_result == -1 or guess_result == 1:
+        # Game over
+        await bot.deleteGame(gid)
+    return
+
+
+# @bot.slash_command()
+@bot.command()
+async def end(ctx):
+    """Ends game in current guild."""
+    gid = ctx.guild.id
+    game = await bot.getGame(gid)
+    word = game.endGame()
+    await ctx.send(f"Game over! The word was {word}")
+    await bot.deleteGame(gid)
+
+
+bot.run(MYTOKEN)
