adfe73b26942262b8b34d0fc17724676cd76d55b
Tyler Zhu <tyler.zhu@berkeley.edu>
2022-01-19 14:01:14 -0800

    update word list to official wordle list of words.

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-with open("words.txt", "r") as f:
+with open("wordle-answers-alphabetical.txt", "r") as f:
     GAME_WORDS = [word.strip().upper() for word in f.readlines()]
 
 with open("sgb-words.txt", "r") as f:
